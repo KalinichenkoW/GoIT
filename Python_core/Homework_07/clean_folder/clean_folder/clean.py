0198a8b1e79fa2154580350611759d6b0a5cf8ad
Volodymyr Kalinichenko <kalinichenko.w@gmail.com>
2023-07-22 20:23:02 +0300

    Заміна на перевірку кількості елементів
    sys.argv - це список значень, в якому завжди присутній перший елемент це повний шлях до файлу програми (тобто елемент з індексом 0)
    тобто якщо ми запустимо скрипт без аргументів sys.argv поверне список з одного значення,
    якщо з аргументами - то кожне значення передане через пробіл буде додано у цей список
    Для прикладу
    import sys
    def main():
        print(f'sys.argv => {sys.argv}')
    if __name__ == '__main__':
        main()
    Далі приклад використання - перший рядок виклик, а наступний рядок результат виконання
    
    python -m example_sys_argv
    sys.argv => ['/home/test/example_sys_argv.py']
    ========
    python -m example_sys_argv one
    sys.argv => ['/home/test/example_sys_argv.py', 'one']
    ===========
    python -m example_sys_argv one two three
    sys.argv => ['/home/test/example_sys_argv.py', 'one', 'two', 'three']
    Тому -
    if sys.argv[1] != "":  такий вираз поверне помилку, якщо викликати без аргументів, тому що ви звертаєтесь до другого елемента у списку, якого не існує (індексація починається з 0) та ще порівнюете з порожньою строкою, яке теж не можливо
    тому вам треба перевіряти кількость елементів, як приклад if len(sys.argv) == 1  - тобто аргумент відсутній
    ============
    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    Якщо у вас виникають будь які складності (навіть дрібні навіть самі примітивні, но які перешкоджають) не соромтесь звертатися особисто у слак до мене  (oleksii frundin)

--- Python_core/Homework_07/clean_folder/clean_folder/clean.py
+++ Python_core/Homework_07/clean_folder/clean_folder/clean.py
@@ -103,18 +103,19 @@ def main(folder: Path):
 
 
 def start():
-    if sys.argv[1] != "":
-        if sys.argv[1]:
-            folder_for_scan = Path(sys.argv[1])
-            print(f"Start in folder: {folder_for_scan.resolve()}")
-            main(folder_for_scan.resolve())
-    else:
-        print("Введіть аргумент")
+    if len(sys.argv) == 1:
+        #    if sys.argv[1]:
+        folder_for_scan = Path(sys.argv[1])
+        print(f"Start in folder: {folder_for_scan.resolve()}")
+        main(folder_for_scan.resolve())
+
+    # else:
+    #     print("Введіть аргумент")
 
     # чи так
 
     # try:
-    #     if sys.argv[1]:
+    #     if len(sys.argv) == 1:
     #         folder_for_scan = Path(sys.argv[1])
     #         print(f"Start in folder: {folder_for_scan.resolve()}")
     #         main(folder_for_scan.resolve())
