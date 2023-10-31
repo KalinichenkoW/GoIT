34e9a0267ea3d514a0d258690cc03242f88c89a1
Volodymyr Kalinichenko <kalinichenko.w@gmail.com>
2023-10-31 23:27:42 +0200

    File main.py

---
+++ Python_web_1/Homework_01/main.py
@@ -0,0 +1,75 @@
+import os
+import webbrowser
+
+from abc import ABC, abstractmethod
+
+# from ContactBook import ContactBook
+# from NoteBook import NoteManager
+# from sort import FileSorter
+# from CryptoPrice import CryptoPriceFetcher
+
+# Базовий клас для модулів
+class BaseClass(ABC):
+    @abstractmethod
+    def run(self):
+        pass
+
+class ContactBook(BaseClass):
+    def run(self):
+        # Тут не зрозумів як додавати   import ContactBook
+
+        # contacts = ContactBook()
+        # contacts.main()
+
+        print("ContactBook functionality")
+
+
+class NoteManager(BaseClass):
+    def run(self):
+        print("NoteManager functionality")
+
+class FileSorter(BaseClass):
+    def run(self):
+        target_folder = input("Enter the folder path to sort: ")
+        print(f"Sorting files in {target_folder}")
+
+class CryptoPriceFetcher(BaseClass):
+    def run(self):
+        print("CryptoPriceFetcher functionality")
+
+class WebSearch(BaseClass):
+    def run(self):
+        query = input("Enter your search query: ")
+        webbrowser.open(f"https://www.google.com/search?q={query}")
+
+class Exit(BaseClass):
+    def run(self):
+        print("Goodbye!") 
+
+def main():
+    modules = {        
+        '1': ContactBook(),
+        '2': NoteManager(),
+        '3': FileSorter(),
+        '4': CryptoPriceFetcher(),
+        '5': WebSearch(),
+        '0': Exit(),        
+    }
+   
+    while True:
+        print("Choose a command:")
+        for key, value in modules.items():
+            print(f"{key}. {value.__class__.__name__}")
+
+        user_input = input("Enter command number: ")
+
+        if user_input in modules:
+            modules[user_input].run()
+           
+            break
+        else:
+            print("Wrong command. Enter a valid command.")
+
+if __name__ == '__main__':
+    main()
+
